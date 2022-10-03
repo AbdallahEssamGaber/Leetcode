0667e49628c85164343a10940212e4d22536f8cf
Sam <70752242+AbdallahEssamGaber@users.noreply.github.com>
2022-10-03 22:52:29 +0200

    Time: 115 ms (66.81%), Space: 44.3 MB (89.42%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    
+    let k = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == nums[i+1]) {
-            nums.splice(i, 1);
-            i--;
+        if (nums[i] !== nums[i+1]) {
+            nums[k] = nums[i];
+            k++;
         }
     }
-
+    return k;
 };
