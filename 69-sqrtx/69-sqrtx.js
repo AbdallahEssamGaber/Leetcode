31505cf218d63cfcb168b4d27443c8538f1a3320
Sam <70752242+AbdallahEssamGaber@users.noreply.github.com>
2022-10-05 22:06:22 +0200

    Time: 152 ms (27.13%), Space: 43.6 MB (66.91%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -3,11 +3,9 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    let i = 1;
-    while (true) {
-        let value = i*i;
-        if (value == x) return i
-        else if (value > x) return i-1
-        i++;
+    let i=1
+    while((i*i)<=x){
+        i++
     }
+    return --i
 };
